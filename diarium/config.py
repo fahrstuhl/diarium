e487ff53a6094e15e27aa9c15a89911f3d6496e4
fahrstuhl <fahrstuhl@mailbox.tu-berlin.de>
2013-02-18 17:41:29 +0100

    implemented configuration via ConfigParser

--- diarium/config.py
+++ diarium/config.py
@@ -7,17 +7,29 @@ import ConfigParser
 import os.path
 
 
-#TODO: Configparser
-config = ConfigParser.SafeConfigParser()
-if(config.read('config.txt') == []):
-    raise 
-dateFormat = config
-timeFormat = "%H:%M:%S"
-fileExtension = ".txt"
-#TODO: Create path on first run
-journalPath = os.path.expanduser("~/journal")
-editor = "/usr/bin/gvim --nofork"
-reader = editor
+class ConfigFileNotFoundAt(Exception):
+    """Exception raised if the config file can not be found.
 
-class Error(Exception):
-    
+    Attributes:
+        path -- path where the config file should have been
+    """
+    def __init__(self, path):
+        self.path = path
+
+    def __str__(self):
+        return "No config file found at " + self.path
+
+configFiles = list()
+codeDirectory = os.path.dirname(__file__)
+configFiles.append(os.path.join(codeDirectory, "config.txt"))
+config = ConfigParser.SafeConfigParser()
+if(config.read(configFiles) == []):
+    raise ConfigFileNotFoundAt(configFiles)
+dateFormat = config.get("journal", "dateFormat", "raw")
+timeFormat = config.get("journal", "timeFormat", "raw")
+fileExtension = config.get("journal", "fileExtension")
+#TODO: Create path on first run
+journalPath = config.get("journal", "journalPath", "raw")
+journalPath = os.path.expanduser(journalPath)
+editor = config.get("journal", "editor")
+reader = config.get("journal", "reader")
