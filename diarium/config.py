ba1c70eb0bc57cd4ddb332062d5a41eb0897e20f
fahrstuhl <fahrstuhl@mailbox.tu-berlin.de>
2013-02-18 23:44:27 +0100

    Display configuration information on first run.

--- diarium/config.py
+++ diarium/config.py
@@ -3,7 +3,11 @@ Created on Jan 27, 2013
 
 @author: fahrstuhl
 '''
-import ConfigParser
+import sys
+if(sys.version[0] == '3'):
+    import configparser as ConfigParser
+else:
+    import ConfigParser
 import os.path
 import appdirs
 
@@ -23,12 +27,7 @@ class ConfigFileNotFoundAt(Exception):
             str += i + "\n"
         return repr(self.str)
 
-config = ConfigParser.ConfigParser()
-configDir = appdirs.user_data_dir("diarium")
-if(not os.path.exists(configDir)):
-    os.mkdir(configDir)
-configFile = os.path.join(configDir, "config.ini")
-if(not os.path.exists(configFile)):
+def firstRun():
     config.add_section("journal")
     config.set("journal", "dateFormat", "%Y-%m-%d")
     config.set("journal", "timeFormat", "%H:%M:%S")
@@ -37,17 +36,42 @@ if(not os.path.exists(configFile)):
     config.set("journal", "reader", "/usr/bin/gvim --nofork")
     config.set("journal", "editor", "/usr/bin/gvim --nofork")
     with open(configFile, "w") as filePointer:
-        config.write(filePointer)
+        config.write(filePointer) 
+    print("""
+    Hi,
+    it seems like this is your first run of diarium.
+    Before you can start, please open
+    {}
+    in a texteditor and set up the commands for your editor and reader and
+    maybe customize your journal directory or whatever else you'd like to change.
+    """.format(configFile))
+    sys.exit()
+
+config = ConfigParser.RawConfigParser({
+                                    "dateFormat": "%Y-%m-%d",
+                                    "timeFormat": "%H:%M:%S",
+                                    "journalPath": "~/journal",
+                                    "fileExtension": ".txt",
+                                    })
+configDir = appdirs.user_data_dir("diarium")
+if(not os.path.exists(configDir)):
+    os.mkdir(configDir)
+configFile = os.path.join(configDir, "config.ini")
+if(not os.path.exists(configFile)):
+    firstRun()
+
 configFiles = list()
 configFiles.append(configFile)
 if(config.read(configFiles) == []):
     raise ConfigFileNotFoundAt(configFiles)
 
-dateFormat = config.get("journal", "dateFormat", "raw")
-timeFormat = config.get("journal", "timeFormat", "raw")
+dateFormat = config.get("journal", "dateFormat")
+timeFormat = config.get("journal", "timeFormat")
 fileExtension = config.get("journal", "fileExtension")
 #TODO: Create path on first run
-journalPath = config.get("journal", "journalPath", "raw")
+journalPath = config.get("journal", "journalPath")
 journalPath = os.path.expanduser(journalPath)
 editor = config.get("journal", "editor")
 reader = config.get("journal", "reader")
+if(not os.path.exists(journalPath)):
+    os.mkdir(journalPath)
